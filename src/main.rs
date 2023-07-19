24aa8882229463e26b093d2500b5d482f68ffb5f
TheWisePigeon <zozozozeph@gmail.com>
2023-07-19 23:40:12 +0000

    minor updates

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
 fn main() {
     let command = std::env::args().nth(1);
     match command {
-        Some(cmd) => {
-            match cmd.as_str() {
-                "init" =>{
-                    //Create json file at ~/.clipper
-                    for (key, value) in std::env::vars_os() {
-                        if key=="HOME" {
-                            if let Some(path) = value.to_str(){
-                                println!("{path}")
-                            }else {
-                                println!("Error while reading your home directory")
-                            }
+        Some(cmd) => match cmd.as_str() {
+            "init" => {
+                let home_dir = "";
+                for (key, value) in std::env::vars_os() {
+                    if key == "HOME" {
+                        if let Some(path) = value.to_str() {
+                            println!("{path}")
+                        } else {
+                            println!("Error while reading your home directory")
                         }
-                        
                     }
-                },
-                _ =>{}
+                }
             }
-        }
+            _ => {}
+        },
         None => {}
     }
 }
