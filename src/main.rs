3623039f635ea83f74334fddef8c015e98483984
TheWisePigeon <zozozozeph@gmail.com>
2023-07-20 08:09:38 +0000

    check for already existing clipper db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{fs::File, path::Path};
+
 fn main() {
     let command = std::env::args().nth(1);
     match command {
@@ -8,8 +10,12 @@ fn main() {
                         if let Some(path) = value.to_str() {
                             //Create clipboard database
                             let clipper_db_path = format!("{path}/.clipperdb.txt");
-                            if let Ok(_) = std::fs::File::create(clipper_db_path) {
-                                println!("Clipper initialized")
+                            if Path::new(&clipper_db_path).is_file() {
+                                println!("An existing Clipper database lives on your system at {clipper_db_path}");
+                                return;
+                            }
+                            if let Ok(_) = File::create(&clipper_db_path) {
+                                println!("Clipper database initialized at {} ", &clipper_db_path)
                             }else {
                                 println!("Error while creating clipboard database")
                             }
